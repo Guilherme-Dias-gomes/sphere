3fa3f533afc653141962c27d9eb59a030e4991c6
Guilherme-Dias-gomes <guilhermediasgomes2@gmail.com>
2024-08-05 20:19:22 -0300

    Priemeiras alterações

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/mode-toggle";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <ModeToggle></ModeToggle>
+      <h1>SPHERE</h1>
+      <Button>Entrar</Button>
+    </main>
+  );
+}
