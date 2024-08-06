da4f0aadc66b5572af080fd949ce85a3b4871909
Guilherme-Dias-gomes <guilhermediasgomes2@gmail.com>
2024-08-05 21:17:22 -0300

    Ajustes no projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         <Image src="/login.png" alt="" width={554} height={832} />
       </aside>
 
-      <section className="space-y-7" flex-col itens-center w-full>
+      <section className="flex flex-col items-center w-full space-y-7">
         <svg width="75" height="75" viewBox="0 0 75 75" fill="none" xmlns="http://www.w3.org/2000/svg">
           <circle cx="37.5" cy="37.5" r="37.5" fill="#E11D48"/>
         </svg>
@@ -24,16 +24,17 @@ export default function Home() {
 
         <Tabs defaultValue="entrar">
           <TabsList>
-            <TabsTrigger value="entrar"></TabsTrigger>
-            <TabsTrigger value="Registrar"></TabsTrigger>
+            <TabsTrigger value="entrar">entrar</TabsTrigger>
+            <TabsTrigger value="registrar">registrar</TabsTrigger>
           </TabsList>
 
           <TabsContent value="entrar">
-            <form action="">
+            <form action="" className="space-y-2">
               <Label htmlFor="email">email</Label>
-              <Input id="email"></Input>
-              <Label htmlFor="Senha">Senha</Label>
-              <Input id="password"></Input>
+              <Input id="email"/>
+              <Label htmlFor="senha">Senha</Label>
+              <Input id="senha" type="password"/>
+              <Button type="submit">Entrar</Button>
             </form>
           </TabsContent>
 
@@ -42,7 +43,7 @@ export default function Home() {
           </TabsContent>
         </Tabs>
 
-        <Button>Entrar</Button>
+        
       </section>
 
     </main>
